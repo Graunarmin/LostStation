75941809067ffff3b7c314df26adbfd7e759403c
Johanna Sacher <johanna.sacher@uni-weimar.de>
2020-01-16 14:25:02 +0100

    fixed observer cam for 3D objects

--- FirstPerson3D/Assets/Scripts/Interactables/Collector.cs
+++ FirstPerson3D/Assets/Scripts/Interactables/Collector.cs
@@ -17,15 +17,35 @@ public class Collector : Interactable
         collectableItem = GetComponent<Collectable>();
     }
 
-
     public override void Interact()
     {
         //on click we collect the item
         Reference.instance.collectHeld = collectableItem;
-        Reference.instance.inventoryDisplay.Activate();
+        //Reference.instance.inventoryDisplay.Activate();
         //Hide the Object Information
         HideInfo();
+        InspectObject();
         //and set it to inactive so it is no longer visible or accessible
         gameObject.SetActive(false);
+    }
+
+    public void InspectObject()
+    {
+        //"duplicate" the clicked on Object
+        GameObject item = Instantiate(gameObject);
+
+        //set the rig as the parent (as in the unity history)
+        item.transform.SetParent(Reference.instance.obsCam.rig);
+
+        //bring it to the middle of the observer Camera's Screen
+        item.transform.localPosition = Vector3.zero;
+
+        //push the model of the Prop down so the pivot is aligned with the "actual" Prop
+        item.transform.GetChild(0).localPosition = Vector3.zero;
+
+        Reference.instance.obsCam.model = item.transform;
+
+        //turn on observer Camera
+        Reference.instance.obsCam.Activate();
     }
 }
