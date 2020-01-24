eb649d75a8371d279a8b8f1ae9d519807b4ddc28
Graunarmin <johanna.sacher@uni-weimar.de>
2020-01-24 21:53:09 +0100

    dialogue works on windows, added Coroutine to start game after intro

--- FirstPerson3D/Assets/Scripts/Menus/IntroSkipButton.cs
+++ FirstPerson3D/Assets/Scripts/Menus/IntroSkipButton.cs
@@ -1,9 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class IntroSkipButton : MonoBehaviour
 {
+
+    void Awake()
+    {
+        StartCoroutine(NextScene());
+    }
+
+    public IEnumerator NextScene()
+    {
+        yield return new WaitForSecondsRealtime(32);
+        LoadNextScene();
+
+    }
     public void SkipVideo()
+    {
+        StopCoroutine(NextScene());
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
