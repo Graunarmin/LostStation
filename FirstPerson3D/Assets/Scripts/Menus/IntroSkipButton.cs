ff7c22e96fb25e2de1a9d7c7370824b0b224b94a
Johanna Sacher <johanna.sacher@uni-weimar.de>
2020-01-24 19:39:42 +0100

    add Intro Video

---
+++ FirstPerson3D/Assets/Scripts/Menus/IntroSkipButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSkipButton : MonoBehaviour
+{
+    public void SkipVideo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
