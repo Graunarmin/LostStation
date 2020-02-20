22f2134166ca2dbd1e4d0616c06301edd62367a5
2 <57009925+JoEL8129@users.noreply.github.com>
2020-02-20 12:38:22 +0100

    Character Update
    added animated Character + script to Call Animations
    not alligned to First Person Camera Yet

---
+++ FirstPerson3D/Assets/FBX/SciFi Character/walker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walker : MonoBehaviour
+{
+    public Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            Anim.SetBool("isWalk", true);
+        }
+        else
+        {
+            Anim.SetBool("isWalk", false);
+
+        }
+
+        if (Input.GetKey("w") && Input.GetKey("left shift"))
+        {
+            
+            Anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            Anim.SetBool("isRunning", false);
+
+        }
+    }
+}
