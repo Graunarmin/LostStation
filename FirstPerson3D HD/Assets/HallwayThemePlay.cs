a8b4c0abd2a9df15888991aa6be94908d5ec6d2d
2 <57009925+JoEL8129@users.noreply.github.com>
2020-04-23 09:11:17 +0200

    Further Animation Update
    no Colliders added yet
    FinalRoom - Finale Animation is still missing

---
+++ FirstPerson3D HD/Assets/HallwayThemePlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayThemePlay : MonoBehaviour
+{
+    public GameObject Theme;
+    public GameObject FadeOutoldThemeTimeline;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Theme.SetActive(false);
+        FadeOutoldThemeTimeline.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Theme.SetActive(true);
+        FadeOutoldThemeTimeline.SetActive(true);
+    }
+}
